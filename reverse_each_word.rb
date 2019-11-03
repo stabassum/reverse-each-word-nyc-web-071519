b20747f85338f64cb6b0b6c2fdda2c9fa0269d5f
github username <github email address>
2019-11-03 14:27:39 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word (string)
+
+    string = string.split('')
+    reverse_string = []
+
+    string.each do |char|
+        reverse_string.unshift(char)
+    end
+
+    return reverse_string.join('')
+/
+end
